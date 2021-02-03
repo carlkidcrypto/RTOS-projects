e96d5034d7ed0b1360f8bb5038ecc23e2960a47d
Carlos <port3116@vandals.uidaho.edu>
2021-02-02 16:40:33 -0800

    LED Semaphore
    -- got the LED semaphore to work.
    -- CNT_TASK will take first and block LED_TASK
    -- LED_TASK will only run when the semaphore is available.

--- assignment4/assignment4/src/main.cpp
+++ assignment4/assignment4/src/main.cpp
@@ -35,38 +35,38 @@ void setup()
       Serial.println(F("DP_SEMAPHORE: Creation Error, not enough heap mem!"));
   }
 
-  BaseType_t xLED_RTVAL = xTaskCreate(
-      LED_TASK, "LED_TASK" // Responsible for blinking LED on pin D13
-      ,
-      128 // Stack size
-      ,
-      (void *) &LED_SEMAPHORE // parameters
-      ,
-      1 // priority
-      ,
-      NULL);
-  
-  if(xLED_RTVAL != pdPASS)
-  {
-    for (;;)
-      Serial.println(F("LED_TASK: Creation Error, not enough heap or stack!"));
-  }
-
   BaseType_t xCNT_RTVAL = xTaskCreate(
       CNT_TASK, "CNT_TASK" // The counter task, 0-42, then 42-0
       ,
       128 // Stack size
       ,
-      (void *) &LED_SEMAPHORE // parameters
+      (void *)LED_SEMAPHORE // parameters
       ,
       1 // priority
       ,
       NULL);
-  
-  if(xCNT_RTVAL != pdPASS)
+
+  if (xCNT_RTVAL != pdPASS)
   {
     for (;;)
       Serial.println(F("CNT_TASK: Creation Error, not enough heap or stack!"));
+  }
+
+  BaseType_t xLED_RTVAL = xTaskCreate(
+      LED_TASK, "LED_TASK" // Responsible for blinking LED on pin D13
+      ,
+      128 // Stack size
+      ,
+      (void *)LED_SEMAPHORE // parameters
+      ,
+      1 // priority
+      ,
+      NULL);
+
+  if (xLED_RTVAL != pdPASS)
+  {
+    for (;;)
+      Serial.println(F("LED_TASK: Creation Error, not enough heap or stack!"));
   }
 
   BaseType_t xDR_RTVAL = xTaskCreate(
@@ -74,13 +74,13 @@ void setup()
       ,
       128 // Stack size
       ,
-      (void *) &DP_SEMAPHORE // parameters
+      (void *)&DP_SEMAPHORE // parameters
       ,
       1 // priority
       ,
       NULL);
-  
-  if(xDR_RTVAL != pdPASS)
+
+  if (xDR_RTVAL != pdPASS)
   {
     for (;;)
       Serial.println(F("DR_TASK: Creation Error, not enough heap or stack!"));
@@ -91,13 +91,13 @@ void setup()
       ,
       128 // Stack size
       ,
-      (void *) &DP_SEMAPHORE // parameters
+      (void *)&DP_SEMAPHORE // parameters
       ,
       1 // priority
       ,
       NULL);
-  
-  if(xDP_RTVAL != pdPASS)
+
+  if (xDP_RTVAL != pdPASS)
   {
     for (;;)
       Serial.println(F("DP_TASK: Creation Error, not enough heap or stack!"));
@@ -109,25 +109,52 @@ void setup()
 
 void LED_TASK(void *pvParameters) // This is a task.
 {
-  (void)pvParameters;
+  // Get pvParameters out
+  SemaphoreHandle_t led_semaphore = *((SemaphoreHandle_t *)pvParameters);
+
+  // initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 
   for (;;)
   {
+    // Let's check if the semaphore is available
+    if (xSemaphoreTake(led_semaphore, 0) == pdTRUE)
+    {
+      Serial.println(F("LED_TASK: semaphore taken!"));
+      // we got it, let us do some stuff
+      byte status = digitalRead(13);
+      if (status)
+        digitalWrite(13, LOW);
+      else
+        digitalWrite(13, HIGH);
+    }
   }
 }
 
 void CNT_TASK(void *pvParameters) // This is a task.
 {
-  (void)pvParameters;
+  // Get pvParameters out
+  SemaphoreHandle_t led_semaphore = *((SemaphoreHandle_t *)pvParameters);
+
+  // First thing we do is take the semaphore
+  xSemaphoreTake(led_semaphore, 0);
 
   for (;;)
   {
+      // we got it, let us do some stuff
+
+      vTaskDelay(pdMS_TO_TICKS(500));
+      // we are done, give back the semaphore
+      Serial.println(F("CNT_TASK: semaphore given!"));
+      xSemaphoreGive(led_semaphore);
   }
 }
 
 void DR_TASK(void *pvParameters) // This is a task.
 {
-  (void)pvParameters;
+  // Get pvParameters out
+  SemaphoreHandle_t *dp_semaphore;
+  dp_semaphore = (SemaphoreHandle_t *)pvParameters;
 
   for (;;)
   {
@@ -136,7 +163,9 @@ void DR_TASK(void *pvParameters) // This is a task.
 
 void DP_TASK(void *pvParameters) // This is a task.
 {
-  (void)pvParameters;
+  // Get pvParameters out
+  SemaphoreHandle_t *dp_semaphore;
+  dp_semaphore = (SemaphoreHandle_t *)pvParameters;
 
   for (;;)
   {
